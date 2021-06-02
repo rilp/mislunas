b572c8d8203e66f2cfedefe4f61b8be82b780f7b
Vic Baruch <victor.baruch@gmail.com>
2021-06-01 23:27:52 -0500

    Public NavBar view

--- src/components/NavMenus.jsx
+++ src/components/NavMenus.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
+import {NavLink} from 'react-router-dom';
+import {AppBar, Toolbar, Button} from '@material-ui/core';
+import Icon from '@mdi/react';
+import { mdiMoonWaningCrescent, mdiAccountCircleOutline } from '@mdi/js';
+import LogoIcon from '../images/logo.svg';
 
 export class NavMenus extends Component {
     render() {
         return (
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                    </IconButton>
-                    <Typography variant="h6">
-                    News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <NavLink exact to="/" activeClassName="not-active" className="HomeLogo">
+                    <Icon path={mdiMoonWaningCrescent} size={1.6} rotate={-12} />
+                        <img src={LogoIcon} alt="Logo Mis Lunas" className="NavBarLogo" />
+                    </NavLink>
+                    <div className="spacer" />
+                    <Button color="inherit" endIcon={<Icon path={mdiAccountCircleOutline} size={1}/>} size="large">Iniciar Sesión</Button>
                 </Toolbar>
             </AppBar>
         )
--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#7672A0"
+    },
+    secondary: {
+      main: "#F48FB1"
+    },
+    bg: {
+      main: "#FDEAEF"
+    }
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
